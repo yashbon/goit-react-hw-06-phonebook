99102deb453a440589213f7dcd7f999d90ef5f29
Bohdan <yashbon@ukr.net>
2023-07-18 16:09:20 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ export const App = () => {
     return (
         <div
             style={{
-                height: '100vh',
+                // height: '100vh',
                 display: 'flex',
                 flexDirection: 'column',
                 justifyContent: 'center',
